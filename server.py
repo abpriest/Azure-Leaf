105bd925f3e8e40ff4b203fc0336bab6b7e546e4
abpriest <abpriest@gmail.com>
2016-10-21 19:41:19 +0000

    copied some boilerplate

---
+++ server.py
@@ -0,0 +1,15 @@
+import os
+import psycopg2
+import psycopg2.extras
+import sys
+reload(sys)
+sys.setdefaultencoding("UTF8")
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+def connectToDB():
+    connectionStr = 'dbname=world user=waldo password=123 host=localhost'
+    try:
+        return psycopg2.connect(connectionStr)
+    except:
+        print("Can't connect to database")
