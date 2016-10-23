a0ff915ac36fb4cb1d05d369a91660b044e5a614
abpriest <abpriest@gmail.com>
2016-10-23 02:15:14 +0000

    you don't get kicked out for refreshing not

--- server.py
+++ server.py
@@ -8,16 +8,23 @@ from flask import Flask, render_template, request
 from db import *
 app = Flask(__name__)
 
+loggedIn = False
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global loggedIn
     if request.method == 'POST':
         if request.form['button'] == 'Sign Up':
             print 'here'
             if not createNewUser(request.form['username'], request.form['password'], 'is_dm' in request.form):
                 return render_template('login.html', message = "That username is taken!")
             else:
+                loggedIn = True
                 return render_template('index.html')
-    return render_template('login.html', message = "")
+    if not loggedIn:
+        return render_template('login.html', message = "")
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host = os.getenv('IP', '0.0.0.0'),
