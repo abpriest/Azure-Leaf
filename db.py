fe2ac7b563c5d8beb49b73bf7b97d465d894479b
James Murphy <james.f.murphy.one@gmail.com>
2016-10-23 00:36:24 +0000

    Fixed a logical error; added createNewUser()

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 # author(s): Taylor Dohmen, Alex Priest, James Murphy
 import psycopg2
 import psycopg2.extras
+from hashlib import md5
 
 def connectToDB():
     connectionStr = 'dbname=world user=waldo password=123 host=localhost'
@@ -18,6 +19,35 @@ def isUserAvailable(username):
         raise Exception("Database connection failed.")
     cur = conn.cursor()
     query = cur.mogrify("SELECT name FROM users WHERE username = %s", (username,))
-    results = cur.execute(query)
+    cur.execute(query)
+    results = cur.fetchall()
     print (not results[0][0]) * "Username '%s' not available!" % username
     return not results[0][0]
+    
+def hashPassword(password, username):
+    """ Returns an md5 hash of `password` with salt from `username` """
+    pw_hash = md5(password)
+    return username[0] + username[-1] + pw_hash
+    
+def createNewUser(username, password, is_dm):
+    """ Calls isUserAvailable() to determine whether it's safe to
+        create a new user with `username`. If it is safe, the new
+        user is created with username `username`, a password salted
+        and hashed from `password`, and `is_dm` determining whether
+        they are a DM. Returns True if successful, false otherwise.
+        
+        If performance is affected by the nested function calls, we
+        should rewrite this to require username availability as a
+        precondition.
+    """
+    
+    if not isUserAvailable(username):
+        return False
+    
+    conn = connectToDB()
+    pw_hash = hashPassword(password, username)
+    cur = conn.cursor()
+    query = cur.mogrify("INSERT INTO users (%s, %s, %s);", username, pw_hash, str(int(is_dm)))
+    cur.execute(query)
+    conn.commit()
+    return True
