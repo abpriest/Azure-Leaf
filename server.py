21e16f865cc0da03f21fadd78352c3fe50b6c50c
abpriest <abpriest@gmail.com>
2016-10-24 16:51:39 +0000

    serving the character gen page

--- server.py
+++ server.py
@@ -43,6 +43,10 @@ def index():
         return render_template('login.html', message = "")
     else:
         return render_template('index.html', username = session['username'])
+        
+@app.route('/characterGen')
+def characterGen():
+    return render_template('characterGen.html')
 
 if __name__ == '__main__':
     app.run(host = os.getenv('IP', '0.0.0.0'),
